c5472a270edefb74bd02cd70167582d6dedae0ea
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-09-07 17:19:40 +0800

    Change the colours

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 
+const myTheme = {
+  "palette": {
+    "success": "#ec5454"
+  }
+}
+
 function MyApp({ Component, pageProps }) {
   return (
-    <GeistProvider>
+    <GeistProvider theme={myTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
